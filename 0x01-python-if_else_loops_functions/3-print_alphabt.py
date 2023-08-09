469edc1c881249a1527411739c388f1bdb3041a3
root <amuMabirimise>
2023-08-09 03:26:01 -0700

    task-3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')+1):
-     if chr(letter) is not 'q' and chr(letter) is not 'e':
-          print("{}".format(chr(letter)), end="")
-
+for char_code in range(ord('a'), ord('z')+1):
+    if chr(char_code) not in ['q', 'e']:
+        print("{}".format(chr(char_code)), end="")
