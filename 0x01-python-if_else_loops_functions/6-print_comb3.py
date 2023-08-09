50ab0313334c93ad2c4547a6ed91e94ffbbffc0f
root <amuMabirimise>
2023-08-09 04:11:51 -0700

    task-3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        print("{:02d}, ".format(first_digit * 10 + second_digit), end='')
+        print("{:d}{:d}".format(first_digit, second_digit), end='')
+        if first_digit == 8:
+            print(", ", end='')
 
-print("{:02d}".format(99))
+print()
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 7-islower.py
+
+def islower(c):
+    """Check if a character is lowercase."""
+    return 97 <= ord(c) <= 122
