c5e6bedd8020c46541d90945cacfa27ba500f525
root <amuMabirimise>
2023-08-11 03:48:51 -0700

    task-1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,11 +6,6 @@ if __name__ == "__main__":
 a = 10
 b = 5
 
-sum_result = add(a, b)
-diff_result = subtract(a, b)
-prod_result = multiply(a, b)
-quotient_result = divide(a, b)
-
 print("Sum of {} and {} = {}".format(a, b, sum_result))
 print("Difference between {} and {} = {}".format(a, b, diff_result))
 print("Product of {} and {} = {}".format(a, b, prod_result))
