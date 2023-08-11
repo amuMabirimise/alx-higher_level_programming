bdc772f8ad4a347f9d314515cd416a50263aeb07
root <amuMabirimise>
2023-08-11 03:59:29 -0700

    task-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    num_args = len(arguments)
+
+    if num_args == 0:
+        print("Number of argument(s) : .")
+    elif num_args == 1:
+        print("Number of argument(s) : 1:")
+        print("1: {}".format(arguments[0]))
+    else:
+        print("Number of argument(s) : {}:".format(num_args))
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, arg))
+
