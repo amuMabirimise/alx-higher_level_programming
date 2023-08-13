a5c6b5fe118cf298205264a94b975bee805e28ac
root <amuMabirimise>
2023-08-13 08:21:47 -0700

    task-0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
 
-def print_list_intager(my_list=[]):
+def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
