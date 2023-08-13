df8ec7f833fb7c7c617477d2eb65f0a0dba16e71
root <amuMabirimise>
2023-08-13 08:06:49 -0700

    task-0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-my_list = [1, 2, 3, 4, 5]
-
-
 def print_list_intager(my_list=[]):
     for num in my_list:
         print("{}".format, (num))
-        print_list_integar(my_list)
