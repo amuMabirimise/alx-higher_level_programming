d09faca0651ed4373f334bc5435125f1946a429f
root <amuMabirimise>
2023-08-16 12:02:32 -0700

    task-7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,3 +2,10 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
+
+my_dictionary = {'a': 1, 'b': 2, 'c': 3}
+update_dictionary(my_dictionary, 'b', 4)
+update_dictionary(my_dictionary, 'd', 5)
+
+for key, value in my_dictionary.items():
+    print("{}: {}".format(key, value))
