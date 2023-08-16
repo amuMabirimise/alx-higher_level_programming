5745faf915ef04370d4341d5df213340f520bfab
root <amuMabirimise>
2023-08-16 09:12:53 -0700

    task-1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
     result_matrix = []
-    
     for row in matrix:
         result_row = []
-        
         for num in row:
-             result_row.append(num ** 2)
-        
-        result_matrix.append(result_row)
-    
-    return result_matrix
+            result_row.append(num ** 2)
+            result_matrix.append(result_row)
+            return result_matrix
