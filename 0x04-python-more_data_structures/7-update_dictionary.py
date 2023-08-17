e50d0d596ad3ccecf739357ea857ff644bd498ec
root <amuMabirimise>
2023-08-17 04:19:15 -0700

    task-7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-my_dictionary = {'a': 1, 'b': 2, 'c': 3}
-update_dictionary(my_dictionary, 'b', 4)  # Update value of existing key
-update_dictionary(my_dictionary, 'd', 5)  # Add new key/value pair
-print(my_dictionary)
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
