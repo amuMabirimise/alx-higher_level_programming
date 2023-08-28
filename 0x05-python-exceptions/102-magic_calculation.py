c09e34b70110645ded3b68f7b176f9c8f90be696
root <amuMabirimise>
2023-08-28 10:08:56 -0700

    task-advanced

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except NameError:
+        except:
             result = b + a
             break
     return result
