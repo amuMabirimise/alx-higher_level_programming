e2229c05d76eae01f1b4270d3163ac5512b4629b
root <amuMabirimise>
2023-08-29 07:45:49 -0700

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining Square module"""
+
+
+class Square:
+    """creating square class"""
+    def __init__(self, size):
+        """Initialization"""
+        self.__size = size
