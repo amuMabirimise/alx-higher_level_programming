a091f689e40c1347c6c0ef94aec9594f6f29f409
root <amuMabirimise>
2023-09-04 03:50:37 -0700

    the_define

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,13 +5,15 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle instance with optional width and height."""
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
+    @property
     def width(self):
         """Retrieve the width of the rectangle."""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """Set the width of the rectangle."""
         if not isinstance(value, int):
@@ -20,10 +22,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """Retrieve the height of the rectangle."""
         return self.__height
 
+    @height.setter
     def height(self, value):
         """Set the height of the rectangle."""
         if not isinstance(value, int):
