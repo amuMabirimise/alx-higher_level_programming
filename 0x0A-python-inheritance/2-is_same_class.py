0d6fe1befd6510e5c9f47dbc76ddba3fee5c3637
root <amuMabirimise>
2023-09-11 08:37:20 -0700

    True or False

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module: is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """A function that returns True:
+    if the object is exactly an instance
+    of the specified class
+    otherwise False
+    """
+    return type(obj) is a_class
