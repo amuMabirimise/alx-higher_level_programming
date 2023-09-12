67870ae6ee83ef4f131bb7d48fe9131103bc2dae
root <amuMabirimise>
2023-09-12 11:33:22 -0700

    task-1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
