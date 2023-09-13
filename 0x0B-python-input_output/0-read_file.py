8d770a2fa3a63d3ecae70f817fb79d00964ad3f9
root <amuMabirimise>
2023-09-13 06:22:42 -0700

    task-0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,11 +2,8 @@
 """defines a readfile function"""
 
 def read_file(filename=""):
+    """refers to a read file function"""
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end='')
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
