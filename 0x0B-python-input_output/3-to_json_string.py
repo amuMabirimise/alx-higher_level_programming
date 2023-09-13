4eded7e02e676535e3b5462b1dba36111d0f52dc
root <amuMabirimise>
2023-09-13 06:52:26 -0700

    task-3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
-
-import json  # Import the json module
-
 def to_json_string(my_obj):
-    """
-    Convert an object to its JSON representation as a string.
+  """Returns the JSON representation of an object (string).
 
-    Args:
-        my_obj: The Python object to be serialized to JSON.
+  Args:
+    my_obj: The object to serialize.
 
-    Returns:
-        str: A JSON string representing the object.
-    """
-    # Use json.dumps to serialize the object to a JSON-formatted string
-    json_string = json.dumps(my_obj)
-    
-    return json_string
+  Returns:
+    The JSON representation of the object.
+  """
+
+  import json
+
+  return json.dumps(my_obj)
