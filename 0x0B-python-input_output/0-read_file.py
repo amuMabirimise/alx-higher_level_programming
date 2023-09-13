f33fbc5d66bccb619d69af4000953cf70b8379aa
root <amuMabirimise>
2023-09-13 06:28:56 -0700

    task-0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a readfile function"""
 
+
 def read_file(filename=""):
     """refers to a read file function"""
         with open(filename, 'r', encoding='utf-8') as file:
