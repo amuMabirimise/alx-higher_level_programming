3fa9741c8d0f13df7954c118f1d5d1fd7804e1f7
root <amuMabirimise>
2023-09-13 07:08:34 -0700

    task-4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
+"""Defines a JSON to object function."""
+import json
+
 
 def from_json_string(my_str):
-  """Returns an object (Python data structure) represented by a JSON string.
-
-  Args:
-    my_str: The JSON string.
-
-  Returns:
-    The object represented by the JSON string.
-  """
-
-  import json
-
-  try:
+    """Return the Python rep a JSON string."""
     return json.loads(my_str)
-  except json.JSONDecodeError:
-    return None
