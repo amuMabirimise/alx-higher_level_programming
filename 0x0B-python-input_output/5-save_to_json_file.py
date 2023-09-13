1c2856602cca0779790d6ea362a8b38dc9eee095
root <amuMabirimise>
2023-09-13 07:23:24 -0700

    task-5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines a JSON writing function."""
+"""from_json_string Module"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """defines a JSON representation."""
-    with open(filename, "w") as l:
-        json.dump(my_obj, l)
+def from_json_string(my_str):
+    """ a function that returns an object
+    represented by a JSON string
+    """
+    return json.loads(my_str)
