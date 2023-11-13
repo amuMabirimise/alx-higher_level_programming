c2033eb970db321356bfc02c7bf8a93b296da8fa
root <amuMabirimise>
2023-11-13 05:56:12 -0800

    fixing semi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const MyArgument = process.argv.length -2;
+const MyArgument = process.argv.length - 2;
 
 if (MyArgument === 0) {
-    console.log('No argument');
-}
-else if (MyArgument === 1) {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (MyArgument === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
