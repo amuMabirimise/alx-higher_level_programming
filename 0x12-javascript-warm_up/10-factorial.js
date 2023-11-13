aef085da53598c4104afa42e5438146de86356da
root <amuMabirimise>
2023-11-13 13:25:42 -0800

    semi

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,18 +2,18 @@
 
 const a = parseInt(process.argv[2]);
 
-function factorial(a) {
-    if (isNaN(a) || a === 1) {
-        return 1;
-    } else {
-        let result = 1;
-        let i = a;
-        while (i > 1) {
-            result *= i;
-            i--;
-        }
-        return result;
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    let i = a;
+    while (i > 1) {
+      result *= i;
+      i--;
     }
+    return result;
+  }
 }
 
 console.log(factorial(a));
