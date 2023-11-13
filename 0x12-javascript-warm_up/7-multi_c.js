7a9668b0346adc0a4587b1baf402b8f4f4bf4f41
root <amuMabirimise>
2023-11-13 13:30:35 -0800

    task-7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
-const x = parseInt(process.argv[2]);
-
-if (!isNaN(x) && x > 0) {
-  let count = 0;
-
-  while (count < x) {
-    console.log('C is fun');
-    count++;
-  }
-} else {
+const x = process.argv[2];
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
