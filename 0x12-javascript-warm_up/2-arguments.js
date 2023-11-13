072c130a009b572e64e22fa3c72a05e4bcc1ff63
root <amuMabirimise>
2023-11-13 03:53:57 -0800

    semi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-if (arguments => 0)
-{
-    console.log("No argument");
-} else
-{
-    console.log("Argument found");
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }
