bff9271bf9e2331b8916797908153914ef2b5b03
root <amuMabirimise>
2023-11-30 05:30:54 -0800

    task-0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Usage: ./get_body_size.sh <URL>
-# Request a URL and display the size of the body of the response
-# Using curl to fetch the response headers, grep for Content-Length, and extract the size
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
 curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
-
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Usage: ./get_response_body.sh <URL>
-# Takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Using curl to send a GET request (-X GET), follow redirects (-L), and display the response body (-s)
+# Takes in a URL, sends a GET request to the URL, displays the body of the response
 curl -sX GET "$1" -L 200
-
