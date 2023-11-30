7418e9148b6e2b2414084a26dcd67d7e12b28f6b
root <amuMabirimise>
2023-11-30 06:07:58 -0800

    cURL Method

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Usage: ./get_response_body.sh <URL>
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Using curl to send a GET request (-X GET), follow redirects (-L), and display the response body (-s)
+curl -sX DELETE "$1"
+
