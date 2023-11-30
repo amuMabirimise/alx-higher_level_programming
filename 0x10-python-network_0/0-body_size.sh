deed12c9d0a3b760ac7a66d3ba3ec3c62ffe86c1
root <amuMabirimise>
2023-11-30 05:25:42 -0800

    task-1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
-# Takes a URL as a parameter
-url="$1"
-
-# Perform the request and store the response in a temporary file
-response_file=$(mktemp)
-curl -s -o "$response_file" "$url"
-
-# Get the size of the response body
-size=$(stat -c '%s' "$response_file")
-
-# Clean up the temporary file
-rm "$response_file"
-
-# Print the size
-echo "$size"
+# Usage: ./get_body_size.sh <URL>
+# Request a URL and display the size of the body of the response
+# Using curl to fetch the response headers, grep for Content-Length, and extract the size
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
 
