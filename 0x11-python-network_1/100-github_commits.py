107e0baa4c59bab65f50c448e557cd71d92b888f
root <amuMabirimise>
2023-12-01 07:14:41 -0800

    advanced

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
+"""
+Python script that lists 10 commits (from the most recent to oldest)
+of a specified repository by a user using the GitHub API.
+
+Uses the requests and sys packages.
+"""
 
 import requests
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}commits"\
-          .format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+
+        # Displaying information about the latest 10 commits
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"Error: Unable to fetch commits. Status code: {response.status_code}")
+
