c76e73eeeb0dedbb9d9f7d944bd9378dc4b02d8f
root <amuMabirimise>
2023-12-01 06:11:49 -0800

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,24 +4,11 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    if len(argv) != 3:
-        print("Usage: {} <repository_name> <owner_name>".format(argv[0]))
-        exit(1)
+    url = "https://api.github.com/repos/{}/{}commits"\
+          .format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
 
-    repository_name = argv[1]
-    owner_name = argv[2]
-
-    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
-
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()
-
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
-    else:
-        print(f"Error: Unable to fetch commits. Status code: {response.status_code}")
-
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
