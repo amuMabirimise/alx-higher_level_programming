53f73b6e3c6181ae200bc7b3921c28cf428ee0ec
root <amuMabirimise>
2023-12-01 06:41:37 -0800

    task-0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,16 +6,12 @@ Uses the urllib package.
 Displays the body of the response with specific information.
 """
 
-import urllib.request
+import urllib.request as request
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", utf8_content)
-
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
