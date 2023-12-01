b31fc5df2e471ccd15740d39401d1112f0a28739
root <amuMabirimise>
2023-12-01 06:09:00 -0800

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""GitHub commits code challenge"""
 
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    # Check if the correct number of command-line arguments is provided
     if len(argv) != 3:
-        print("Usage: {} <owner> <repo>".format(argv[0]))
+        print("Usage: {} <repository_name> <owner_name>".format(argv[0]))
         exit(1)
 
-    owner = argv[1]
-    repo = argv[2]
+    repository_name = argv[1]
+    owner_name = argv[2]
 
-    # Construct the URL for the GitHub API request
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
 
-    # Send a GET request to the GitHub API
     response = requests.get(url)
 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         commits = response.json()
 
-        # Display information about the latest 10 commits
         for commit in commits[:10]:
             sha = commit.get('sha')
             author_name = commit.get('commit').get('author').get('name')
