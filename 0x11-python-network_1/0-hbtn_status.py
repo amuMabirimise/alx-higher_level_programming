d16b37e54d8bbda036eeb82a915db42b297b182c
root <amuMabirimise>
2023-12-01 07:21:29 -0800

    task-0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 Displays the body of the response using urllib.
 """
 
-import urllib.requestt
+import urllib.request
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
