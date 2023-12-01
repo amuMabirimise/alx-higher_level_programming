5fc2193a7e65ce067fa3d704ee7dc621a7b20c2e
root <amuMabirimise>
2023-12-01 06:56:58 -0800

    task-0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-""" Script that fetches from a url using urllib package"""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+Displays the body of the response using urllib.
+"""
 
-import urllib.request as request
+import urllib.request
 
-if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))`
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", utf8_content)
+
