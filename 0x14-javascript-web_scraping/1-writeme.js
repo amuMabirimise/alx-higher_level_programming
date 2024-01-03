2b1584781ff363782714a9e0287f88e3e896d17b
root <amuMabirimise>
2024-01-03 04:52:45 -0800

    task-1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,19 +2,12 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.error('Usage: node 1-writeme.js <file-path> <content>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log('File written successfully.');
   }
 });
 
